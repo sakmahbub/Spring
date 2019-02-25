7b0b51c001530b797cdcdedcae4c0ceb11a4bf0a
mahbub <sakmahbub@gmail.com>
2019-02-25 23:58:17 +0600

    some update class and Home Work

---
+++ LaunchEticket/launcheticket/src/main/java/com/mahbub/launcheticket/LauncheticketApplication.java
@@ -0,0 +1,13 @@
+package com.mahbub.launcheticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LauncheticketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LauncheticketApplication.class, args);
+    }
+
+}
