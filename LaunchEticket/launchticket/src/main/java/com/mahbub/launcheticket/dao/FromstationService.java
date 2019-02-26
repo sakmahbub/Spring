2ba57ecd51cc2d30c82e4493e4f63642d9d660af
Sakil Mahbub <sakmahbub@gmail.com>
2019-02-26 13:19:13 +0600

    some update

---
+++ LaunchEticket/launchticket/src/main/java/com/mahbub/launcheticket/dao/FromstationService.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mahbub.launcheticket.dao;
+
+
+import com.mahbub.launcheticket.model.Fromstation;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author noman
+ */
+@Service
+public interface FromstationService {
+     public String insertFromStationService(Fromstation fstation);
+
+    public String updateFromStationService(int f_id, Fromstation fstation);
+
+    public String deleteFromtationService(int f_id);
+
+    public String viewFromStationService();
+
+    public Fromstation viewOneFromStationService(int f_id);
+}
