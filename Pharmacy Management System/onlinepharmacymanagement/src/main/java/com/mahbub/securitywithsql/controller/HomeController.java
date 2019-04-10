680135ab0cbd4bb55e1623e3ec8becf72a4fef96
Sakil Mahbub <sakmahbub@gmail.com>
2019-04-10 13:05:19 +0600

    some update

--- Pharmacy Management System/onlinepharmacymanagement/src/main/java/com/mahbub/securitywithsql/controller/HomeController.java
+++ Pharmacy Management System/onlinepharmacymanagement/src/main/java/com/mahbub/securitywithsql/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.mahbub.securitywithsql.controller;
 
 
 
+import com.mahbub.securitywithsql.repo.SalesRepo;
 import com.mahbub.securitywithsql.repo.SummaryRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,15 +10,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+
 @Controller
 public class HomeController {
 
     @Autowired
     private SummaryRepo summaryRepo;
-
+    @Autowired
+    private SalesRepo salesRepo;
 
     @GetMapping(value = "/")
     public String dashboardView(Model model) {
+        double taka[]=new double[salesRepo.findAll().size()];
+        for(int i=0; i < salesRepo.findAll().size(); i++){
+            taka[i]=salesRepo.findAll().get(i).getTotalPrice();
+        }
+        model.addAttribute("monthlyRevnue",taka);
         return "dashboard";
 
     }
